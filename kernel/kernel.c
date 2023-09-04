e6a6260c11600d97f455be2cacc8d64aed252940
Goedix <goedix+inazucraft@wearehackerone.com>
2023-09-05 01:01:02 +0200

    Feat: Trying to write with VGA text mode buffer

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,16 @@
+void kprintf(unsigned char* str);
+
+void kprintf(unsigned char *str) {
+    char* video = (char*) 0xb8000;
+
+    for (int i = 0; str[i] != '\0'; ++i) {
+        video[i * 2] = str[i];
+        video[i * 2 + 1] = 0x07;
+    }
+}
+
+
 // Kernel entry point
 void _start(void) {
-    
+    kprintf("Hello world!\n");
 }
